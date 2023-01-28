6dbc61a605341e0221056ff827b50c56f464b86d
fauzi dev <fauziagst04@gmail.com>
2023-01-28 12:57:07 +0700

    add

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func main() {
+	// nomor 1 narsistik
+	fmt.Println("Naricissistic")
+	fmt.Println(narcissistic(153))
+	fmt.Println(narcissistic(111))
+
+	fmt.Println("====================")
+
+	//nomor 2 parity outlier
+	fmt.Println("Parity Outlier")
+	ex1 := []int{2, 4, 0, 100, 11, 2602, 36}
+	ex2 := []int{160, 3, 1719, 19, 11, 13, -21}
+	ex3 := []int{11, 13, 15, 19, 9, 13, -21}
+	fmt.Println(parityOutlier(ex1))
+	fmt.Println(parityOutlier(ex2))
+	fmt.Println(parityOutlier(ex3))
+
+	fmt.Println("====================")
+}
+
+func narcissistic(x int) bool {
+	arr := splitToGigits(x)
+	total := 0
+
+	lenA := len(arr)
+
+	for _, v := range arr {
+		total += int(math.Pow(float64(v), float64(lenA)))
+	}
+
+	if x == total {
+		return true
+	} else {
+		return false
+	}
+}
+
+func splitToGigits(n int) []int {
+	var ret []int
+
+	for n != 0 {
+		ret = append(ret, n%10)
+		n /= 10
+	}
+
+	reverseInt(ret)
+
+	return ret
+}
+
+func reverseInt(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func parityOutlier(x []int) string {
+
+	countOdd := 0
+	countEven := 0
+	keyodd := 0
+	keyeven := 0
+
+	for i := 0; i < len(x); i++ {
+		if x[i]%2 == 0 {
+			countEven++
+			keyeven = i
+		} else {
+			countOdd++
+			keyodd = i
+		}
+	}
+
+	if countEven == 1 {
+		return strconv.Itoa(x[keyeven])
+	} else if countOdd == 1 {
+		return strconv.Itoa(x[keyodd])
+	} else {
+		return "false"
+	}
+}
